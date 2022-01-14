fcdf44d2500aee138e35cd0b0da115c9a1de8d99
gitrishabhshukla <rishabhbiet2016@gmail.com>
2022-01-14 14:53:50 +0530

    basic structure

---
+++ src/main/java/com/practice/requestresponsedblogging/RequestResponseDbLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.requestresponsedblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestResponseDbLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestResponseDbLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/requestresponsedblogging/RequestResponseDbLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.requestresponsedblogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestResponseDbLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
